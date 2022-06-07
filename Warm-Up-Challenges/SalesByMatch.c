c1332b900aa15c7b14a70f9b1114946fa9819f13
Rishabh S Chauhan <rishabh007singh@gmail.com>
2022-06-07 20:29:37 +0530

    Solution for Sales by Match ran successfully

--- Warm-Up-Challenges/SalesByMatch.c
+++ Warm-Up-Challenges/SalesByMatch.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, ar[101];
+    scanf("%d", &n);
+    for (int i = 0; i < 101; i++)
+    {
+        ar[i] = 0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int j;
+        scanf("%d", &j);
+        ar[j] += 1;
+    }
 
+    int total =0;
+    for (int i = 0; i < 101; i++)
+    {
+        if(ar[i]>0)
+        {
+            total += ar[i]/2;
+        }
+    }
+    printf("%d", total);
+    return 0;
+    
+}
