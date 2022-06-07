1bdb10024375f5bb8433ad4856682199685e7916
Rishabh S Chauhan <rishabh007singh@gmail.com>
2022-06-07 20:46:32 +0530

    Solution for 2D Array - DS ran successfully

--- Arrays/2DArray-DS.c
+++ Arrays/2DArray-DS.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int arr[6][6], i, j, max;
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
 
+    int hourglass[4][4];
+
+        for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            hourglass[i][j] = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+        }
+    }
+
+    max = hourglass[0][0];
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if(hourglass[i][j]>max)
+                max = hourglass[i][j];
+        }
+        
+    }
+    
+    printf("%d", max);
+
+    return 0;
+}
