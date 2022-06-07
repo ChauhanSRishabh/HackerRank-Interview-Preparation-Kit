3e0daf76ccb32795313d97c35df07de024283f20
Rishabh S Chauhan <rishabh007singh@gmail.com>
2022-06-07 21:03:12 +0530

    Solution for Arrays: Left Rotation ran successfully
    A spelling mistake from the previous commit was rectified
    More Comments were included to properly explain the logic

--- Arrays/ArraysLeftRotation.c
+++ Arrays/ArraysLeftRotation.c
@@ -3,14 +3,14 @@
 void left_rotate(int n, int d, int *a)
 {
     int temp;
-    for (int i = 0; i < d; i++)  //runs as many times as Left Roattion needs to be done, i.e., d times
+    for (int i = 0; i < d; i++)  //runs as many times as Left Rotation needs to be done, i.e., d times
     {
-        temp = a[0];
-        for (int j = 0; j < n-1; j++)
+        temp = a[0]; //storing the 1st element of the array in a variable named temp before Left Rotating so it doesn't get lost
+        for (int j = 0; j < n-1; j++) //shifts every element to the left and runs till penultimate index, we know why!
         {
             a[j] = a[j + 1];
         }
-        a[n-1] = temp;
+        a[n-1] = temp; // last index gets the element at index 0 that was stored in temp before Left Rotation
     }
 
     for (int i = 0; i < n; i++)
